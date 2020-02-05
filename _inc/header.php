5e70b114ac94df6c69fc4f275f43568034108d02
andysaktia <37647600+andysaktia@users.noreply.github.com>
2020-02-05 14:14:16 +0700

    Update header.php

--- _inc/header.php
+++ _inc/header.php
@@ -3,7 +3,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <title>LAI.SABDA.ORG</title>
+    <title>Your website</title>
 
     <!-- Bootstrap core CSS -->
 <link href="lib/bootstrap/css/bootstrap.min.css" type="text/css"rel="stylesheet">
@@ -20,7 +20,7 @@
   <body>
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <div class="container">
-      <a class="navbar-brand" href="#">LAI.SABDA.ORG</a>
+      <a class="navbar-brand" href="#">Your website name</a>
       <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -28,10 +28,10 @@
       <div class="navbar-collapse collapse" id="navbarsExampleDefault" style="">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link js-link" href="#">Home <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link js-link" href="#">Link</a>
           </li>
           <li class="nav-item">
             <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
@@ -39,9 +39,9 @@
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
             <div class="dropdown-menu" aria-labelledby="dropdown01">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
+              <a class="dropdown-item js-link" href="#">Action</a>
+              <a class="dropdown-item js-link" href="#">Another action</a>
+              <a class="dropdown-item js-link" href="#">Something else here</a>
             </div>
           </li>
         </ul>
